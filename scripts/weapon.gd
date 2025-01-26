7eef746d7d6f73469e3882c94e6b1dfb7d5ddbde
Star2578 <nuttykingger@gmail.com>
2025-01-26 21:36:56 +0700

    build more stage

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -3,25 +3,25 @@ extends RigidBody2D
 var torque_strength: float = 1000.0  # Torque for spinning the weapon
 var max_angular_speed: float = 10.0  # Limit for angular velocity
 var damping: float = 0.9  # General damping for angular velocity
-var dash_force:float = 1.25
+var dash_force: float = 1000
 #var torque_direction = 1 # rotation pivot (1 or -1)
 
-var can_dash:bool = true
+var can_dash: bool = true
+var cannot_dash = "#00fff3" # Sprite modulate color while cannot dash
 
-var init_pos
+var init_pos: Vector2
 
-var rotation1
-var rotation2
+var sprite: Sprite2D
+var rotation1: CollisionShape2D
+var rotation2: CollisionShape2D
 var last_touch_point: Node2D
-
-var prev_mouse_pos
 
 func _ready():
 	init_pos = position
+	sprite = $Sprite
 	rotation1 = $Area2D/Rotation1
 	rotation2 = $Area2D/Rotation2
 	last_touch_point = rotation1
-	prev_mouse_pos = get_global_mouse_position()
 
 func _process(delta):
 	# For debugging
@@ -29,14 +29,27 @@ func _process(delta):
 	var mouse_pos = get_global_mouse_position()
 	var to_mouse = mouse_pos - last_touch_point.global_position
 	
+	
 	#reset position
 	if (Input.is_key_pressed(KEY_R)):
 		position = init_pos
 	
 	#dash
-	if (Input.is_key_pressed(KEY_SHIFT) && can_dash):
-		apply_impulse(to_mouse.normalized() * 1000)
+	if Input.is_action_just_pressed("dash") and can_dash:
+		print("prepare-dash")
+		sprite.modulate = "#ffffff"
+		smooth_timescale(0.05, 0.1)
+		
+	if Input.is_action_just_released("dash") and can_dash:
+		print("activate-dash")
+		sprite.modulate = cannot_dash
+		Engine.time_scale = 1.0
+		get_viewport().get_camera_2d().zoom = Vector2(0.45, 0.45)
+		apply_impulse(to_mouse.normalized() * dash_force)
 		can_dash = false
+		
+	if (can_dash == false && Engine.time_scale != 1.0):
+		Engine.time_scale = 1.0
 	
 	# trigger once when LMB pressed down
 	#if (Input.is_action_just_pressed("ui_select")):
@@ -46,8 +59,6 @@ func _process(delta):
 	var angle_diff = wrapf(angle_to_mouse - rotation, -PI, PI)
 	var torque = angle_diff * torque_strength
 	apply_torque_impulse(torque)
-
-	prev_mouse_pos = mouse_pos
 	
 	if abs(angular_velocity) > max_angular_speed:
 		angular_velocity = sign(angular_velocity) * max_angular_speed
@@ -56,7 +67,15 @@ func _integrate_forces(state: PhysicsDirectBodyState2D):
 	angular_velocity *= damping
 
 func reset_dash():
+	sprite.modulate = "#ffffff"
 	can_dash = true;
 
 func _on_touch_ground(body):
 	reset_dash()
+	
+func smooth_timescale(target_time_scale: float, in_time: float):
+	var camera = get_viewport().get_camera_2d()
+	var tween_t = create_tween()
+	var tween_c = create_tween()
+	tween_t.tween_property(Engine, "time_scale", target_time_scale, in_time)
+	tween_c.tween_property(camera, "zoom", Vector2(0.5, 0.5), in_time)
