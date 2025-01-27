5235f4ea40721f8dde1258c183eb61552d3c62ad
Star2578 <nuttykingger@gmail.com>
2025-01-27 19:03:56 +0700

    main menu upgrade

---
+++ scripts/game_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+var bgm_vol = 1.0
+var sfx_vol = 1.0
+var bgm_muted: bool = false
+var sfx_muted: bool = false
+
+func _ready():
+	print("GameManager Ready!")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,13 @@
 extends Control
 
+func _ready():
+	$TextureRect/AnimationPlayer.play("bg")
+
 func to_test_scene():
 	get_tree().change_scene_to_file("res://levels/building_level.tscn")
 	
 func quit():
 	get_tree().quit()
+
+func on_bgm_finished():
+	$AudioStreamPlayer2D.play()
